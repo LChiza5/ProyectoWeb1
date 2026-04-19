61fc71a941c84e2dd5c0fe0fe40d58cc7a9a5b10
Chiza <lualvarezva@est.utn.ac.cr>
2026-04-19 00:56:26 -0600

    Se crea el ErrorMessage para que muestre errores de forma reutilizable, ya sea como que no se pudo cargar la API, un error de red o no hay preguntas disponibles

--- ProyectoTriviaWeb/src/components/ErrorMessage.jsx
+++ ProyectoTriviaWeb/src/components/ErrorMessage.jsx
@@ -1,3 +1,9 @@
-export default function ErrorMessage() {
-  return <div>ErrorMessage</div>;
+export default function ErrorMessage({ mensaje }) {
+  if (!mensaje) return null;
+
+  return (
+    <div className="error-message">
+      <p>⚠️ {mensaje}</p>
+    </div>
+  );
 }
