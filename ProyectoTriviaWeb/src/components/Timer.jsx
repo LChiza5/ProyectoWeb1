81a4806e4e4f150e078029080c8f50f9864edd44
Chiza <lualvarezva@est.utn.ac.cr>
2026-04-18 22:41:14 -0600

    Se crea el componente Timer.jsx. Esto con la funcion de poder llevar el tiempo para cada respuesta, utilizando los hooks respectivos como para el contero regresivo, reiniciar el timepo cuando cambia la pregunta y guardar el tiempo que va cambiando.

--- ProyectoTriviaWeb/src/components/Timer.jsx
+++ ProyectoTriviaWeb/src/components/Timer.jsx
@@ -1,3 +1,28 @@
-export default function Timer() {
-  return <div>Timer</div>;
+import { useEffect, useState } from "react";
+
+export default function Timer({ tiempoInicial, alTerminar, reset }) {
+  const [tiempo, setTiempo] = useState(tiempoInicial);
+
+  useEffect(() => {
+    setTiempo(tiempoInicial);
+  }, [reset, tiempoInicial]);
+
+  useEffect(() => {
+    if (tiempo <= 0) {
+      if (alTerminar) alTerminar();
+      return;
+    }
+
+    const intervalo = setInterval(() => {
+      setTiempo((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(intervalo);
+  }, [tiempo, alTerminar]);
+
+  return (
+    <div>
+      <h3>Tiempo: {tiempo}s</h3>
+    </div>
+  );
 }
