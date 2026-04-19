8f776a8242ea4ec2c39b5783216dbd5d707fb084
Chiza <lualvarezva@est.utn.ac.cr>
2026-04-19 00:52:28 -0600

    Se crea el componente CategoryCard para reutilizar los selects de categoría y dificultad.

--- ProyectoTriviaWeb/src/components/CategoryCard.jsx
+++ ProyectoTriviaWeb/src/components/CategoryCard.jsx
@@ -1,3 +1,30 @@
-export default function CategoryCard() {
-  return <div>CategoryCard</div>;
+export default function CategoryCard({
+  label,
+  id,
+  value,
+  onChange,
+  options
+}) {
+  return (
+    <p className="form-group">
+      <label className="form-label" htmlFor={id}>
+        {label}
+      </label>
+
+      <select
+        id={id}
+        className="form-select-custom"
+        value={value}
+        onChange={onChange}
+      >
+        <option value="">— Seleccionar —</option>
+
+        {options.map((opt, index) => (
+          <option key={index} value={opt.value}>
+            {opt.label}
+          </option>
+        ))}
+      </select>
+    </p>
+  );
 }
