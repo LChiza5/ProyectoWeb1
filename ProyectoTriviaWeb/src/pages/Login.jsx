33bf1f5b56c830f0f0defe03b8c997ca4ad60ac9
Sebastian Villega Barquero <sebastianbarquerocr@gmail.com>
2026-04-28 17:55:11 -0600

    Se agrega login con GitHub
    Se habilita github en firebase.console y se implementa login con gitHub

--- ProyectoTriviaWeb/src/pages/Login.jsx
+++ ProyectoTriviaWeb/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { auth } from "../utils/firebase";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider,GithubAuthProvider, signInWithPopup } from "firebase/auth";
 
 export default function Login() {
   const navigate = useNavigate();
@@ -20,6 +20,20 @@ export default function Login() {
       setCargando(false);
     }
   };
+
+  const loginGithub = async () => {
+  setCargando(true);
+  setError("");
+  try {
+    const provider = new GithubAuthProvider();
+    const result = await signInWithPopup(auth, provider);
+    navigate("/home", { state: { usuario: result.user.displayName || result.user.email}});
+  } catch {
+    setError("No se pudo iniciar sesión con GitHub.");
+    setCargando(false);
+  }
+};
+
 
   return (
     <main className="login-page">
@@ -52,6 +66,31 @@ export default function Login() {
             </>
           )}
         </button>
+
+        <button
+          className="btn-google"
+          onClick={loginGithub}
+          disabled={cargando}
+          aria-busy={cargando}
+        >
+          {cargando ? (
+            "Iniciando sesión..."
+          ) : (
+            <>
+          <svg
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              aria-hidden="true"
+              >
+            <path d="M12 .5C5.65.5.5 5.65.5 12a11.5 11.5 0 0 0 7.86 10.92c.58.1.79-.25.79-.56v-2.17c-3.2.7-3.88-1.54-3.88-1.54-.53-1.33-1.28-1.69-1.28-1.69-1.05-.72.08-.71.08-.71 1.15.08 1.76 1.18 1.76 1.18 1.03 1.76 2.69 1.25 3.35.96.1-.74.4-1.25.72-1.54-2.55-.29-5.23-1.28-5.23-5.7 0-1.26.45-2.3 1.18-3.11-.12-.29-.51-1.46.11-3.04 0 0 .96-.31 3.15 1.19a10.9 10.9 0 0 1 5.74 0c2.19-1.5 3.15-1.19 3.15-1.19.62 1.58.23 2.75.11 3.04.73.81 1.18 1.85 1.18 3.11 0 4.43-2.69 5.4-5.25 5.69.41.35.78 1.05.78 2.12v3.15c0 .31.21.67.8.56A11.5 11.5 0 0 0 23.5 12C23.5 5.65 18.35.5 12 .5z"/>
+          </svg>
+
+      Continuar con GitHub
+    </>
+  )}
+</button>
       </div>
     </main>
   );
